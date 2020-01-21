a6809c124811417d73d195e414b08e9cb18f3b2a
zack-opp47 <58990407+zack-opp47@users.noreply.github.com>
2020-01-21 16:15:56 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -50,8 +50,8 @@ if CONFIG_CHECK:
     quit(1)
 
 # Telegram App KEY and HA
-API_KEY = os.environ.get("API_KEY", "1035344")
-API_HASH = os.environ.get("API_HASH", "df2edaa430e6117850b77878fd092b37")
+API_KEY = os.environ.get("API_KEY", "1155176")
+API_HASH = os.environ.get("API_HASH", "f05e45113d3fc5d4ff985da0f750d5e5")
 
 # Userbot Session String
 STRING_SESSION = os.environ.get("STRING_SESSION", None)
